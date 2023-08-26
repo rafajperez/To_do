a019552333e4107bb023324364a46de3b68e4b62
rafaeljperez <rafael_cordas@hotmail.com>
2023-08-26 13:16:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+//css styles
+import styles from './App.module.css'
+//components
+import Footer from './components/Footer';
+import Header from './components/Header';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+//Interface
+import { ITask } from './interfaces/Task';
+import Modal from './components/Modal';
+
+function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (title: string): void => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.title !== title;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task)
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+
+    const updatedTask: ITask = { id, title, difficulty }
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task
+    })
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+
+  }
+
+  return (
+    <div>
+      <Modal
+        title='Editar Tarefa'
+        children={
+          <TaskForm
+            btnText='Editar'
+            taskList={taskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      />
+      < Header />
+      <main className={styles.main}>
+        <div className={styles.todo_form}>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm
+            taskList={taskList}
+            setTaskList={setTaskList}
+            btnText='Cadastrar'
+          />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask} />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
